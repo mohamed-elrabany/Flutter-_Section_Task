0a0483cb64bc2474cd2964ecb3c8d47ef47faa9b
Mohamed Loay <elrabany1234@gmail.com>
2025-05-19 01:12:35 +0300

    Profile layout structure

--- lib/profile/profile_page/profile_page.dart
+++ lib/profile/profile_page/profile_page.dart
@@ -21,88 +21,183 @@ class _ProfilePageState extends State<ProfilePage> {
         title: const Text("Profile"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Center(
-            child: Consumer<UserModel>(
-              builder: (context, userModel, child){
-               return Stack(
-                alignment: Alignment.bottomRight,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.grey,
-                    radius: 100,
-                    child: userModel.user?.image == null
-                        ? Icon(Icons.person, size: 200, color: Colors.white38)
-                        : ClipOval(child: Image.file(
-                      userModel.user!.image!,
-                      fit: BoxFit.cover,
-                      height: 200,
-                      width: 200,)),
-                  ),
-                  CircleAvatar(
-                    backgroundColor: Colors.black,
-                    radius: 25,
-                    child: IconButton(
-                      onPressed: () {
-                        showModalBottomSheet(
-                          context: context,
-                          builder: (context) =>
-                              SizedBox(
-                                height: 200,
-                                child: Column(
-                                  children: [
-                                    const Text("Profile",
-                                        style: TextStyle(fontSize: 25)),
-                                    const Divider(),
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment
-                                          .spaceEvenly,
-                                      children: [
-                                        Options(
-                                          onPressed: () {
-                                            userModel.imageSelector(ImageSource.camera);
-                                            Navigator.pop(context);
-                                          },
-                                          title: "Camera",
-                                          icon: Icons.camera_alt,
-                                        ),
-                                        Options(
-                                          onPressed: () {
-                                            userModel.imageSelector(ImageSource.gallery);
-                                            Navigator.pop(context);
-                                          },
-                                          title: "Gallery",
-                                          icon: Icons.image,
-                                        ),
-                                        if(userModel.user?.image != null)
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Consumer<UserModel>(
+                builder: (context, userModel, child){
+                 return Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      radius: 100,
+                      child: userModel.user?.image == null
+                          ? Icon(Icons.person, size: 200, color: Colors.white38)
+                          : ClipOval(child: Image.file(
+                        userModel.user!.image!,
+                        fit: BoxFit.cover,
+                        height: 200,
+                        width: 200,)),
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.black,
+                      radius: 25,
+                      child: IconButton(
+                        onPressed: () {
+                          showModalBottomSheet(
+                            context: context,
+                            builder: (context) =>
+                                SizedBox(
+                                  height: 200,
+                                  child: Column(
+                                    children: [
+                                      const Text("Profile",
+                                          style: TextStyle(fontSize: 25)),
+                                      const Divider(),
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment
+                                            .spaceEvenly,
+                                        children: [
                                           Options(
-                                            selectedImage: userModel.user?.image,
                                             onPressed: () {
-                                              userModel.removeImage();
+                                              userModel.imageSelector(ImageSource.camera);
                                               Navigator.pop(context);
                                             },
-                                            title: "delete",
-                                            icon: Icons.delete,
+                                            title: "Camera",
+                                            icon: Icons.camera_alt,
                                           ),
-                                      ],
-                                    ),
-                                  ],
+                                          Options(
+                                            onPressed: () {
+                                              userModel.imageSelector(ImageSource.gallery);
+                                              Navigator.pop(context);
+                                            },
+                                            title: "Gallery",
+                                            icon: Icons.image,
+                                          ),
+                                          Options(
+                                            onPressed: () {
+
+                                            },
+                                            title: "Edit",
+                                            icon: Icons.edit,
+                                          ),
+                                          if(userModel.user?.image != null)
+                                            Options(
+                                              selectedImage: userModel.user?.image,
+                                              onPressed: () {
+                                                userModel.removeImage();
+                                                Navigator.pop(context);
+                                              },
+                                              title: "delete",
+                                              icon: Icons.delete,
+                                            ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                        );
-                      },
-                      icon: const Icon(
-                          Icons.camera_alt, size: 35, color: Colors.grey),
-                    ),
-                  )
-                ],
-              );
-               },
+                          );
+                        },
+                        icon: const Icon(
+                            Icons.camera_alt, size: 35, color: Colors.grey),
+                      ),
+                    )
+                  ],
+                );
+                 },
+              ),
             ),
-          )
-        ],
+        
+            Container(
+              width: double.infinity, // Ensures the container stretches full width
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start, // Align all children to the start (left)
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Email"),
+                        Text("email"),
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Password"),
+                        Text("*******"),
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Name"),
+                        Text("Mohamed Loay"),
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Phone"),
+                        Text("+201011993597"),
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Gender"),
+                        Text("Male"),
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [Text("Dark Mode"),
+                          IconButton(
+                          onPressed: (){
+
+                          },
+                          icon: Icon(Icons.dark_mode_outlined))
+                        ],),
+                      ],
+                    ),
+                  ),
+
+        
+                ],
+              ),
+            )
+        
+        
+          ],
+        ),
       ),
+
     );
   }
 }
